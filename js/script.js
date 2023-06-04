ebeaa48fa6d7796af2e52d9fb0f5d4c74f95df73
weydson <weydsonlino98@gmail.com>
2023-06-04 00:46:28 -0300

    origin/init

--- js/script.js
+++ js/script.js
@@ -14,66 +14,97 @@
             var maiorMenor = document.querySelector('.maiorMenor')
             var chances = document.querySelector('.chances')
             var palpitesAnteriores = document.querySelector('.palpitesAnteriores')
+            palpitesAnteriores.style.color = "red"
             /*Aqui é definido a quantidade de palpites que o úsuario tem e 
             o resultado da consulta do número informado*/ 
             var resultado = document.querySelector('.ultimoResultado')
-            var palpiteQuant = 1
+            var palpiteQuant = 0
             var palpiteMax = 10
+            var ultimoPalpite
 
             campoPalpite.focus()
             chances.textContent = `Você tem até ${palpiteMax} tentativas`
             
+            
             function checarPalpite(){
-
                 var palpiteUsuario = Number (campoPalpite.value)
-                palpitesAnteriores.textContent += `${palpiteUsuario}  `
+                
+                        resultado.textContent = ""
+                         if (ultimoPalpite === palpiteUsuario) {
+                            resultado.textContent=`Digite um número diferente do anterior`
+                            campoPalpite.focus()
+                            campoPalpite.value = ''
+                         }
+                        else{
+                            erros()
 
+                            if (palpiteUsuario === numeroAleatorio) {
+                                resultado.textContent = `Parabéns, o número era ${numeroAleatorio}`;
+                                resultado.style.backgroundColor = 'green';
+                                resultado.style.color= 'white';
+                                maiorMenor.textContent = ' '
+                                chances.textContent = ' '
+                                fimdejogo();
+                            }
+                            //O palpite foi errado
+                            else{
+                                resultado.textContent = 'Você Errou'
+                                resultado.style.color = "white";
+                                resultado.style.backgroundColor = 'red';
+                                
+                                //Palpite maior e diferente do anterior
+                                if (palpiteUsuario > numeroAleatorio) {
+                                    maiorMenor.textContent = `O número digitado é maior"`;
+                                    
+                                }
+                                //Palpite menor e diferente do anterior 
+                                else if(palpiteUsuario < numeroAleatorio){
+                                    maiorMenor.textContent = 'O número digitado é menor'   
+                                }
 
-                if (palpiteUsuario === numeroAleatorio) {
-                    resultado.textContent = 'Parabéns';
-                    resultado.style.backgroundColor = 'green';
-                    resultado.style.color= 'white';
-                    maiorMenor.textContent = ' '
-                    chances.textContent = ' '
-                    fimdejogo();
-                }
-                else if (palpiteQuant >= palpiteMax){
-                    resultado.textContent = 'Você Perdeu'
-                    resultado.style.backgroundColor = 'red';
-                    resultado.style.color= 'white';
-                    maiorMenor.textContent = ' '
-                    chances.textContent = ' '
-                    fimdejogo();
-                    
-                }
-                else{
-                    resultado.textContent = 'Você Errou'
-                    resultado.style.backgroundColor = 'red';
-                    chances.textContent = `Agora lhe restam ${10 - palpiteQuant} palpites`
-
-                    if (palpiteUsuario > numeroAleatorio) {
-                       
-                        maiorMenor.textContent = "O número digitado é maior"
-                     
-                    } else {
-                        maiorMenor.textContent = 'O número digitado é menor'
+                                palpitesAnteriores.textContent += `${palpiteUsuario},  `
+                                palpiteQuant ++
+                                chances.textContent = `Agora lhe restam ${10 - palpiteQuant} palpites`
+                                
+                            }
+                        
+                            ultimoPalpite = palpiteUsuario
+                            campoPalpite.value = ""
+                            campoPalpite.focus()
+                            checarchance()
+                        
+                        
                     }
-                    
                 }
+
+            
+            function erros(){
+                if (palpitesAnteriores.textContent === '') {
+                    palpitesAnteriores.textContent  = `Números usados : `
+                }
+            }
                 
-                palpiteQuant ++
-                campoPalpite.value = ""
-                campoPalpite.focus()
-                
+            function checarchance(){
+                if (palpiteQuant === (palpiteMax))
+                {
+                    resultado.textContent = `Você Perdeu o número era ${numeroAleatorio}`
+                    resultado.style.backgroundColor = 'red';
+                    resultado.style.color= 'white';
+                    maiorMenor.textContent = ' '
+                    chances.textContent = ' '
+                    fimdejogo();
+                }
             }
 
             function fimdejogo(){
+                
                 campoPalpite.disabled = true
                 enviarPalpite.disabled = true
                 campoPalpite.value = ' '
                 botaoReinicio = document.createElement('Button')
+                botaoReinicio.classList.add('meuBotao')
                 botaoReinicio.textContent = 'Iniciar novo Jogo'
-                document.body.appendChild(botaoReinicio)
+                document.getElementById("jogo").appendChild(botaoReinicio)
                 botaoReinicio.addEventListener('click' , reiniciarJogo)
 
             }
@@ -81,7 +112,8 @@
             function reiniciarJogo(){
                 campoPalpite.disabled = false
                 enviarPalpite.disabled = false
-                palpiteQuant = 1
+                palpiteQuant = 0
+                
 
                 botaoReinicio.parentNode.removeChild(botaoReinicio);
 
@@ -92,7 +124,11 @@
                 }
 
                 chances.textContent = `Você tem até ${palpiteMax} tentativas`
+                palpitesAnteriores.textContent = ''
+                numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+                campoPalpite.focus()
             }
+            
             
             
             enviarPalpite.addEventListener('click', checarPalpite);
